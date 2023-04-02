56e93fdf187d493fe49dfa267f8f92b3235a8383
PavleProd <pavleprod249@gmail.com>
2023-04-02 19:29:12 +0200

    add Row Transposition Encryption

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from random import sample
 
 
-def caesarEncription(plaintext: str, offset: int) -> str:
+def caesarEncryption(plaintext: str, offset: int) -> str:
     """
-    Cezarov algoritam koji pomera svaki karakter originalnog teksta za neki pomeraj unapred po engleskom alfabetu.
+    Cezarov algoritam koji pomera svaki karakter originalnog teksta za neki pomeraj unapred po engleskom alfabetu.\n
     :param plaintext: originalni tekst
     :param offset pomeraj u engleskom alfabetu
     :return: enkriptovani tekst.
@@ -17,9 +17,9 @@ def caesarEncription(plaintext: str, offset: int) -> str:
     return ''.join(newString)
 
 
-def monoalphabeticEncription(plaintext: str, key=None) -> tuple[str, str]:
+def monoalphabeticEncryption(plaintext: str, key=None) -> tuple[str, str]:
     """
-    Monoalfabetski algoritam preslikava svaki karakter originalnog teksta za ASCII vrednost permutacije alfabeta
+    Monoalfabetski algoritam preslikava svaki karakter originalnog teksta za ASCII vrednost permutacije alfabeta\n
     :param plaintext: originalni tekst
     :param key: opciono neka permutacija alfabeta, kao string
     :return: tuple(enkriptovani tekst, ascii permutacija)
@@ -42,7 +42,7 @@ def _createPlayfairEntry(plaintext: str, equalChars: tuple[chr, chr]) -> list[tu
     Od plaintexta, pravi lowercase, alphabet listu parnjaka. Nealfabetski karakteri se preskacu
     Specijalni slucajevi:
     1. Neparna velicina: paranjak postaje (plaintext[-1], x)
-    2. Parnjaci su isti kaakteri: pravimo dva parnjaka (x, char) i (char, x)
+    2. Parnjaci su isti kaakteri: pravimo dva parnjaka (x, char) i (char, x)\n
     :param plaintext: originalni tekst
     :param equalChars: par karaktera koji se preslikavaju u isti(prvi karakter)
     :return: Lista parnjaka koju dalje obradjuje playfair algoritam
@@ -90,12 +90,12 @@ def _printMatrix(matrix: list[list[chr]]):
         print()
 
 
-def playfairEncription(plaintext: str, key: str, equalChars=None) -> str:
+def playfairEncryption(plaintext: str, key: str, equalChars=None) -> str:
     """
     Playfair algoritam radi sa 5x5 matricom u sledecim koracima:
     Prvi korak: Upisujemo kljuc u matricu karakter po karakter tako da ne ponavljamo nijedno slovo
     Drugi korak: Prepisujemo ostatak alfabeta, ali pritom preskacemo slova koja se vec pojavljuju.
-    Posto engleski alfabet ima 26 slova, potrebno je dva karaktera preslikati u jedan
+    Posto engleski alfabet ima 26 slova, potrebno je dva karaktera preslikati u jedan\n
     :param plaintext: originalni tekst
     :param key: kljuc
     :param equalChars: Karakteri koji se preslikavaju u jedan(prvi), podrazumevano (i, j)
@@ -146,12 +146,52 @@ def playfairEncription(plaintext: str, key: str, equalChars=None) -> str:
     return ''.join(result)
 
 
+def rowTranspositionEncryption(plaintext: str, numberOfCols: int, key=None) -> tuple[str, str]:
+    """
+    Row Transposition upisuje redom originalni tekst u matricu, sa zadatim brojem kolona. U slucaju da se ne moze
+    popuniti ceo red, dodaje se znak x. Onda se na osnovu kljuca koji je jedna permutacija brojeva od 1..numberOfCols
+    bira kojim ce redom biti ispisan tekst(na primer kolona 1 i key[0] = 3, to znaci da ce kolona 1 ici treca)\n
+    :param key: opciono permutacija brojeva u obliku stringa, duzina i brojevi treba da budu od 1..numberOfCols
+    :param plaintext: originalni tekst
+    :param numberOfCols: broj kolona u transpozicionoj matrici
+    :return:
+    """
+    if key is None:
+        key = ''.join(str(i) for i in sample(range(1, numberOfCols + 1), numberOfCols))  # permutacija brojeva od 0..numberOfCols-1
+
+    plaintext = ''.join([c.lower() for c in plaintext if c != ' '])  # uklanjamo razmake, ostali znaci dozvoljeni
+    if numberOfCols > len(plaintext) or len(key) != numberOfCols:
+        return "", ""  # greska
+    numberOfRows = len(plaintext) // numberOfCols
+    if len(plaintext) % numberOfCols != 0:
+        numberOfRows += 1
+    matrix = [['']*numberOfCols for i in range(numberOfRows)]
+    curr = 0
+    for i in range(numberOfRows):  # pravljenje matrice
+        for j in range(numberOfCols):
+            if curr >= len(plaintext):
+                matrix[i][j] = 'x'  # ako nema vise originalnog teksta dodajemo x za dopunu
+            else:
+                matrix[i][j] = plaintext[curr]
+            curr += 1
+    cypher = []
+    for j in range(1, numberOfCols + 1):  # kljuc indeksiran od 1
+        nextIndex = key.index(str(j))  # nadjemo na kojem se indeksu se nalazi i, ta kolona se sledeca ubacuje
+        for i in range(numberOfRows):
+            cypher.append(matrix[i][nextIndex])
+    return ''.join(cypher), key
+
+
 if __name__ == '__main__':
     text = "Napadamo u podne ako ne bude vetra"
-    res = caesarEncription(text, 3)
+    res = caesarEncryption(text, 3)
     print(res)
     textKey = "qwertzuiopasdfghjklyxcvbnm"
-    res = monoalphabeticEncription(text, textKey)
+    res = monoalphabeticEncryption(text, textKey)
     print(res)
-    res = playfairEncription(text, "vetrobran")
+    res = playfairEncryption(text, "vetrobran")
+    print(res)
+    res = rowTranspositionEncryption("That's what she said", 4, "3142")
+    print(res)
+    res = rowTranspositionEncryption(res[0], 4, "3142")  # dupla transpozicija
     print(res)
